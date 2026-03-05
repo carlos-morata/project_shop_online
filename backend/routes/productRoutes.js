c0dbcaad128659ec1e172d99f1b95ec9fe777b16
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-03-05 16:15:13 +0100

    del/endpoints-productsGender&Category

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -14,16 +14,6 @@ router.get('/', productControllers.getProducts);
 // http://localhost:3000/productos/buscar?query=camiseta
 router.get('/buscar', productControllers.searchProducts);
 
-// http://localhost:3000/genero/mujer
-// router.get('/genero/:gender', productControllers.getByGender);
-
-// http://localhost:3000/categorias/mujer
-// http://localhost:3000/categorias/hombre
-// router.get('/categorias/:gender', productControllers.getCategoriesByGender);
-
-// http://localhost:3000/mujer/abrigos
-// router.get('/genero/:gender/categoria/:category', productControllers.getProductsByGenAndCat);
-
 // http://localhost:3000/mujer/abrigos/2
 router.get('/:gender/:category/:product_id', productControllers.getProductById);
 
