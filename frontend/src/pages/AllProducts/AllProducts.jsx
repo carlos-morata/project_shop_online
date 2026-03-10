eaebd8bd3ba0661ad262dbc683a7a84b96d0855f
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-03-10 13:29:31 +0100

    add/AllProductsStylesFile

--- frontend/src/pages/AllProducts/AllProducts.jsx
+++ frontend/src/pages/AllProducts/AllProducts.jsx
@@ -3,19 +3,27 @@ import { Link, useParams } from "react-router-dom";
 import axios from 'axios';
 
 const AllProducts = () => {
-  const { gender } = useParams();
+  const { gender, category } = useParams();
   const [ products, setProducts ] = useState([]);
   const [ totalProducts, setTotalProducts ] = useState(0);
+  const [ currentPage, setCurrentPage ] = useState(1);
+  const [ totalPages, setTotalPages ] = useState(1);
+
+  // Devolver a la página 1.
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [gender, category]);
 
   useEffect(() => {
     const fetchAllProducts = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/api/products?gender=${gender}`);
+        const response = await axios.get(`http://localhost:3000/api/products?gender=${gender}&page=${currentPage}`);
 
         setProducts(response.data.products || []);
 
         if(response.data.pagination) {
           setTotalProducts(response.data.pagination.total);
+          setTotalPages(response.data.pagination.totalPage);
         }
 
       } catch (error) {
@@ -23,12 +31,20 @@ const AllProducts = () => {
       }
     }
     fetchAllProducts()
-  }, [gender])
+  }, [gender, category, currentPage]);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  }
+  
+  const handleNextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  }
 
   return <section className="products-container">
-    <p>Mostrando <strong>{totalProducts}</strong> productos</p>
+    <p className="showProducts-text">Mostrando <strong>{totalProducts}</strong> productos</p>
     { products.map((item) => (
-      <Link key={item.product_id} to={`/${gender}/${item.category.toLowerCase()}/${item.product_id}`}>
+      <Link key={item.product_id} to={`/${gender}/${item.category?.toLowerCase()}/${item.product_id}`}>
         <article className="product-article">
           <img src={item.url_image} alt={item.name} />
           <h3>{item.name}</h3>
@@ -36,6 +52,24 @@ const AllProducts = () => {
         </article>
       </Link>
     ))}
+
+    { totalPages > 1 && (
+      <section className="pagination-container">
+        <button 
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+          className="btn-pagination">
+            Anterior
+        </button>
+        <p>Página {currentPage} de {totalPages}</p>
+        <button 
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className="btn-pagination">
+            Siguiente
+        </button>
+      </section>
+    )}
   </section>;
 };
 
