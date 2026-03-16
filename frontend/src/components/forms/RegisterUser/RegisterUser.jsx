30833ab53b3623a3e07c20d783960959a5030dec
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-03-16 18:06:27 +0100

    change/className-userForm

--- frontend/src/components/forms/RegisterUser/RegisterUser.jsx
+++ frontend/src/components/forms/RegisterUser/RegisterUser.jsx
@@ -49,27 +49,27 @@ const RegisterUser = () => {
   }
 
   return <section className="userForm-container">
-    <h2 className="register-title"> <span>Crear</span> Cuenta</h2>
+    <h2 className="user-title"> <span>Crear</span> Cuenta</h2>
 
     {error && <p>{error}</p>}
 
-    <form className="register-form" action="" onSubmit={handleSubmit}>
-      <label className="register-label" htmlFor="username">Nombre de Usuario</label>
-      <input className="register-input" type="text" name="username" id="username" placeholder="pepeMorales" required onChange={handleChange} />
+    <form className="user-form" action="" onSubmit={handleSubmit}>
+      <label className="user-label" htmlFor="username">Nombre de Usuario</label>
+      <input className="user-input" type="text" name="username" id="username" placeholder="pepeMorales" required onChange={handleChange} />
 
-      <label className="register-label" htmlFor="email">Correo Eléctronico</label>
-      <input className="register-input" type="email" name="email" id="email" placeholder="pepe@gmail.com" required onChange={handleChange} />
+      <label className="user-label" htmlFor="email">Correo Eléctronico</label>
+      <input className="user-input" type="email" name="email" id="email" placeholder="pepe@gmail.com" required onChange={handleChange} />
 
-      <label className="register-label" htmlFor="password">Contraseña</label>
-      <input className="register-input" type="password" name="password" placeholder="******" required onChange={handleChange} />
+      <label className="user-label" htmlFor="password">Contraseña</label>
+      <input className="user-input" type="password" name="password" placeholder="******" required onChange={handleChange} />
 
-      <label className="register-label" htmlFor="repitePassword">Repite la Contraseña</label>
-      <input className="register-input" type="password" name="repitePassword" placeholder="******" required onChange={handleChange} />
+      <label className="user-label" htmlFor="repitePassword">Repite la Contraseña</label>
+      <input className="user-input" type="password" name="repitePassword" placeholder="******" required onChange={handleChange} />
 
-      <button className="register-button" type="submit">Crear Cuenta</button>
+      <button className="user-button" type="submit">Crear Cuenta</button>
     </form>
 
-    <p className="login-text">¿Ya tienes una cuenta?
+    <p className="user-text">¿Ya tienes una cuenta?
     <Link to="/inicioSesión">Inicia Sesión</Link>
     </p>
   </section>;
