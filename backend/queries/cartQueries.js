7173fa1a9a15d94c13ae5483612ee3b1d2491005
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-03-26 22:19:13 +0100

    fix/query

--- backend/queries/cartQueries.js
+++ backend/queries/cartQueries.js
@@ -1,7 +1,10 @@
 const queries = {
     addProductToCart: 
     ` INSERT INTO cart (user_id, product_id, quantity, size)
-        VALUES ($1, $2, $3, $4);`
+        VALUES ($1, $2, $3, $4)
+        ON CLONFLICT (user_id, product_id, size)
+        DO UPDATE SET quantity = cart.quantity + EXCLUDED.quantity
+        RETURNING *;`
 }
 
 module.exports = queries;
