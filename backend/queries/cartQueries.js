b5123b0138ec1abd661941f0d8d6d57ab93e6b37
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-03-27 18:59:14 +0100

    add/query

--- backend/queries/cartQueries.js
+++ backend/queries/cartQueries.js
@@ -4,7 +4,19 @@ const queries = {
         VALUES ($1, $2, $3, $4)
         ON CONFLICT (user_id, product_id, size)
         DO UPDATE SET quantity = cart.quantity + EXCLUDED.quantity
-        RETURNING *;`
+        RETURNING *;`,
+    
+    getProductsCart:
+    ` SELECT
+        c.quantity
+        c.size
+        p.product_id
+        p.name
+        p.price
+        p.url_image
+     FROM cart c
+     JOIN products p ON c.product_id = p.product_id
+     WHERE c.user_id = $1; `
 }
 
 module.exports = queries;
