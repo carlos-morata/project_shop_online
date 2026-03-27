547c9ce224b74aad098d8e0737102e6d05294808
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-03-27 19:03:14 +0100

    add/functionProductsCart

--- backend/models/cartModels.js
+++ backend/models/cartModels.js
@@ -1,15 +1,27 @@
 const pool = require("../config/db_sql");
 const queries = require("../queries/cartQueries");
 
+// Añadir productos al carrito
 const addProductToCartModel = async (user_id, product_id, quantity, size) => {
     try {
-        const resultCart = await pool.query(queries.addProductToCart, [user_id, product_id, quantity, size]);
+        const resultCart = await pool.query(queries.addProductToCart, [ user_id, product_id, quantity, size ]);
         return resultCart.rows;
     } catch (error) {
         throw error;
     }
 }
 
+// Leer productos en el carrito
+const getProductsCart = async (user_id) => {
+    try {
+        const resultProductsCart = await pool.query(queries.getProductsCart, [ user_id ]);
+        return resultProductsCart.rows
+    } catch (error) {
+        throw error;
+    }
+}
+
 module.exports = {
-    addProductToCartModel
+    addProductToCartModel,
+    getProductsCart
 }
