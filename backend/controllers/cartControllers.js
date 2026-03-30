b8dede75e4e5f6a9b4aa8ff6f4649b4ea1a4b03e
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-03-30 21:03:21 +0200

    add/functionUpdateQuantity

--- backend/controllers/cartControllers.js
+++ backend/controllers/cartControllers.js
@@ -44,7 +44,26 @@ const getProductsCart = async (req, res) => {
     }
 }
 
+// - / + Cantidad del carrito
+const updateQuantity = async(req, res) => {
+    const user_id = req.user.userId;
+    const { product_id, size } = req.body;
+
+    try {
+        const resultUpdateQuantity = await cartModels.updateQuantityModel(user_id, product_id, size);
+
+        res.status(200).json({
+            success: true,
+            data: resultUpdateQuantity
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Error interno del servidor" });
+    }
+}
+
 module.exports = {
     addProductToCart,
-    getProductsCart
+    getProductsCart,
+    updateQuantity
 }
