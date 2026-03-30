8bd8c816b6ac97a0be556d9a3286f4441bddca38
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-03-30 20:48:08 +0200

    add/shoppingCart

--- frontend/src/pages/ShoppingCartPage/ShoppingCartPage.jsx
+++ frontend/src/pages/ShoppingCartPage/ShoppingCartPage.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const ShoppingCartPage = () => {
-  return <div>ShoppingCartPage</div>;
+  const [ cart, setCart ] = useState([]);
+
+  useEffect(() => {
+    const fetchCart = async () => {
+      try {
+        const token = localStorage.getItem('token');
+
+        if(!token) {
+          alert("Para ver tús productos en la cesta, necesitas iniciar sesion.");
+          return;
+        }
+
+        const response = await axios.get(`http://localhost:3000/api/cart/`, 
+          { headers: { Authorization: `Bearer ${token}` } });
+
+        setCart(response.data.data);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchCart();
+  }, []);
+
+  const totalProductsCart = cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
+
+
+  return <section className="shoppingCart-container">
+    <div className="shoppingCart-top">
+    <h2 className="title-cart">Mi cesta</h2>
+    <Link className="shopping-link" to='/'>Seguir Comprando</Link>
+    </div>
+    <p className="articleCart-text">
+      Tienes {totalProductsCart} { totalProductsCart === 1 ? 'artículo seleccionado' : 'artículos seleccionados' }
+    </p> 
+    <hr />
+    { cart.length === 0 ? ( <p>Tú cesta está vacía.</p> ) : (
+      cart.map((item, index) => (
+        <article key={index} className="productCart-article">
+            <img src={item.url_image} alt={item.name} className="productCart-image" />
+            <h3 className="productCart-title">{item.name}</h3>
+            <p>Talla: {item.size}</p>
+            <p>{item.quantity}</p>
+            <p className="productCart-price">{item.price} €</p>          
+        </article>
+      ))
+    )}
+  </section>;
 };
 
 export default ShoppingCartPage;
