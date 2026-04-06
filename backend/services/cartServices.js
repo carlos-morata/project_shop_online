fbc4b6dedffc75c1508420e5ab24cad99e791101
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-04-07 01:25:41 +0200

    fix/functionCartSErvices2

--- backend/services/cartServices.js
+++ backend/services/cartServices.js
@@ -2,7 +2,6 @@ const cartModels = require('../models/cartModels');
 
 const deleteProductCartServices = async (cart_id) => {
     const resultDelete = await cartModels.deleteProductCartModel(cart_id);
-    console.log(resultDelete)
     
     if(resultDelete.rowCount === 0) throw new Error('Error al borrar producto del carrito')
         else return resultDelete;
