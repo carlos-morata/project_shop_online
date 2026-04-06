0b123980f45c5e0a155cfea02259359eb45eed22
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-04-06 16:43:57 +0200

    nameClass

--- frontend/src/components/common/FreeReturns/FreeReturns.jsx
+++ frontend/src/components/common/FreeReturns/FreeReturns.jsx
@@ -4,8 +4,8 @@ import { Truck } from 'lucide-react';
 
 const FreeReturns = () => {
   return <section className="freeReturns-section">
-    <h5><Truck /> Devoluciones Gratuitas</h5>
-    <p>¿No te queda bien? Tienes 30 días para devolverlo sin coste alguno. <Link to="">Saber más</Link></p>
+    <h5 className="freeReturns-title"><Truck /> Devoluciones Gratuitas</h5>
+    <p className="freeReturns-text">¿No te queda bien? Tienes 30 días para devolverlo sin coste alguno. <Link to="">Saber más</Link></p>
   </section>;
 };
 
