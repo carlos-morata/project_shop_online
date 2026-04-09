5bf0f3b1c7edd6af32f1f604f6f7f599b063f5e2
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-04-09 16:46:44 +0200

    add/Order&OrderItemsQueries

--- backend/queries/ordersQueries.js
+++ backend/queries/ordersQueries.js
@@ -1,8 +1,16 @@
 const queries = {
+    // Añadir pedido
     addOrder:
-    ` INSERT INTO orders (user_id, total_price, state)
+    ` INSERT INTO orders(user_id, total_price, state)
         VALUES($1, $2, $3)
         RETURNING order_id;`,
+    
+    // Añadir items del pedido
+    addOrderItems:
+    ` INSERT INTO order_items(order_id, product_id, quantity, price)
+        VALUES($1, $2, $3, $4)
+        RETURNING *;`
+    
 }
 
 module.exports = queries;
