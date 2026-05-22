d77fef552798fd8a162d8f1f113b2c350b89caa1
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-05-22 16:58:25 +0200

    add/orderRoute

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -3,4 +3,7 @@ const router = express.Router();
 const ordersControllers = require("../controllers/ordersControllers");
 const verifyToken = require("../middlewares/verifytoken");
 
+// Crear pedido
+router.post('/add', verifyToken, ordersControllers.addOrder);
+
 module.exports = router;
