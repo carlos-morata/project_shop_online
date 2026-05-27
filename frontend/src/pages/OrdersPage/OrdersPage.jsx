4ced95cd991fd5654e94f68c5e9dee8ede67ce6d
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-05-27 13:33:13 +0200

    add/orderLogicFront

--- frontend/src/pages/OrdersPage/OrdersPage.jsx
+++ frontend/src/pages/OrdersPage/OrdersPage.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 const OrdersPage = () => {
-  return <div>OrdersPage</div>;
+
+  const [ order, setOrder ] = useState([]);
+
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        const token = localStorage.getItem('token');
+
+        if(!token) {
+          alert("Para ver tus pedidos, necesistar iniciar sesión.");
+          return;
+        }
+
+        const response = await axios.get(`${VITE_API_URL}/api/order/`, 
+          { headers: { Authorization: `Bearer ${token}` } });
+
+          setOrder(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchOrder();
+  }, []);
+
+  return <section>
+
+  </section>;
 };
 
 export default OrdersPage;
