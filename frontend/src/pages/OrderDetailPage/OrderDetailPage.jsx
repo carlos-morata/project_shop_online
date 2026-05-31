a031a5094d5b5fdff4da1a18ce5a2405a95e2ac1
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-05-31 22:59:51 +0200

    feat/addOrderDetail&OrderItems

--- frontend/src/pages/OrderDetailPage/OrderDetailPage.jsx
+++ frontend/src/pages/OrderDetailPage/OrderDetailPage.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from 'axios';
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+// Importaciones componentes de orders
+import Orderinfo from '../../components/orders/OrderInfo';
+import OrderItems from '../../components/orders/OrderItems';
+import OrderSummary from '../../components/orders/OrderSummary';
+import OrderTracking from '../../components/orders/OrderTracking';
 
 const OrderDetailPage = () => {
-  return <div>OrderDetailPage</div>;
+  const [ orderDetail, setOrderDetail ] = useState([]);
+  const { order_id } = useParams();
+
+  useEffect(() => {
+    const fetchOrderId = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if(!token) {
+          alert("Para ver tus pedidos, necesistar iniciar sesión.");
+          return;
+        }
+
+        const response = await axios.get(`${VITE_API_URL}/api/order/${order_id}`,
+          { headers: { Authorization: `Bearer ${token}` } });
+          setOrderDetail(response.data.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      fetchOrderId();
+    }, [order_id]);
+
+  return <section>
+    <h3>Pedido #{order_id}</h3>
+    {/* <p>
+      { orderDetail[0]?.created_date && new Date(orderDetail[0].created_date).toLocaleDateString('es-ES', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric' 
+      }) }
+    </p> */}
+    <OrderItems products={orderDetail[0]?.products}/>
+  </section>;
 };
 
 export default OrderDetailPage;
