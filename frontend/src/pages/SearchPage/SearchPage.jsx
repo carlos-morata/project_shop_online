3b42523b48347fb6e932fd3f4cd0c134ca8bdda2
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2026-07-03 17:37:41 +0200

    refactor/searchProducts

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import api from '../../config/axiosInstance';
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const SearchPage = () => {
-  const { query } = useParams();
   const [ results, setResults ] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchSearchProducts = async () => {
-      const response = await api.get(`/api/products/buscar?query=${query}`);
+  const handleSubmit = async () => {
+    const response = await api.get(`/api/products/buscar?query=${searchValue}`)
+    setResults(response.data);
+  }
 
-      setResults(response.data);
-    };
-    fetchSearchProducts();
-  }, [query]);
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  }
 
   const handleProductDetails = (item) => {
     const url = `/${item.gender}/${item.category}/${item.product_id}`;
@@ -24,10 +26,17 @@ const SearchPage = () => {
   }
 
   return <section className="search-container">
-    <h1>Resultados de Búsqueda {query}</h1>
+    <div className="searchInput-container">
+      <input className="search-input" type="text" value={searchValue} onChange={handleChange} placeholder="Buscador de Productos" />
+      <button type="submit" className="search-btn" onClick={handleSubmit}>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="links" />
+      </button>
+    </div>
+    <h1>Resultados de Búsqueda Para {searchValue}</h1>
 
     {results.length === 0 && <p>Cargando...</p>}
 
+    <div className="product-container">
     {results.map((item) => (
       <article key={item.product_id} className="product-article">
       <img src={item.url_image} alt={item.description} title={item.name} />
@@ -37,6 +46,7 @@ const SearchPage = () => {
       <button onClick={() => handleProductDetails(item)}>Ver Detalles</button>
       </article>
     ))}
+    </div>
   </section>;
 };
 
